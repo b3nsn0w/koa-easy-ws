570d389a917af584fa080a76b9f5bdf995148055
Yunyu Lin <mail@yunyul.in>
2020-07-10 14:09:14 -0700

    Support arbitrary PropertyName

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,11 @@ import WebSocket, { ServerOptions } from "ws";
 import http from "http";
 import { Middleware } from "koa";
 
-export type WebSocketContext = { ws?: () => Promise<WebSocket> };
-declare function webSocket(
-  propertyName?: "ws",
-  options?: { server?: http.Server; wsOptions?: ServerOptions }
-): Middleware<{}, WebSocketContext>;
+export type WebSocketContext<PropertyName extends string = "ws"> = { [ws in PropertyName]?: () =>  Promise<WebSocket> };
 
-export default webSocket;
+declare function websocket<PropertyName extends string = "ws">(
+  propertyName?: PropertyName,
+  options?: { server?: http.Server; wsOptions?: ServerOptions }
+): Middleware<{}, WebSocketContext<PropertyName>>;
+
+export default websocket;
